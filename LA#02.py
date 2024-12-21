bc2d23e488badc199519348b8cbce5a60712d669
Aly-Bcna <alyssabacunawa2004@gmail.com>
2024-12-21 11:43:19 +0800

    LA#02.py

---
+++ LA#02.py
@@ -0,0 +1,14 @@
+LA#02
+
+class Mlhero():
+  def init(self, name, role):
+  self.name = name
+  self.role = role
+
+support = Mlhero("Angela", "Support")
+support = Mlhero("Saber", "assassin")
+
+print(support.name)
+print(support.role)
+print(assassin.name)
+print(assassin.role)
