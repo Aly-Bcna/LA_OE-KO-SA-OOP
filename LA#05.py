aec50b05864c2affbd347ecff0fee1705089dd74
Aly-Bcna <alyssabacunawa2004@gmail.com>
2024-12-21 11:52:34 +0800

    LA#05.py

---
+++ LA#05.py
@@ -0,0 +1,15 @@
+LA#05
+
+class Mlhero():
+   def init(self, name, role):
+   self.name = name
+   self.role = role
+
+   def describe(self):
+     print(f"{self.name} is a/an {self.role} hero.")
+     
+support = Mlhero("Angela", "support")
+marksman = Mlhero("Miya", "Marksman:)
+
+support.describe()
+marksman.describe()
