e2174377f91c5b3feaac59f76f21d608f75ffba4
Aly-Bcna <alyssabacunawa2004@gmail.com>
2024-12-21 12:07:40 +0800

    LA#11.py

---
+++ LA#11.py
@@ -0,0 +1,16 @@
+LA#11
+
+class Phone:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def describePhone(self):
+    print(f"{self.brand} is a {self.model} model")
+    
+class Android:
+  def __init__(self, brand, model):
+  phone.init(self, brand, model)
+
+samsung = Phone("Samsung", "A14")
+samsung.describePhone()
